77b2b1c8c1a4d402c709c1c482be34bc134835df
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-09-11 14:15:05 +0300

    fixed test

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,9 +1,13 @@
 import GameSavingLoader from '../GameSavingLoader';
+import read from '../reader';
+import json from '../parser';
 
 describe('work class GameSavingLoader', () => {
   test('work async promise load()', async () => {
     const expected = '{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}';
-    const received = GameSavingLoader.load().then((saving) => saving);
+    const data = await read();
+    const value = await json(data);
+    const received = value;
     expect(received).toEqual(expected);
   });
 });
